9651de84a19bd1cb9bb912c0ae74fe82103c2ca6
Leonardo Guerrini <90059757+LeonardoGuerrini@users.noreply.github.com>
2024-09-02 11:36:32 -0300

    V-1.0

---
+++ carrinho.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Carrinho | Wissen Livraria</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">Wissen</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Meu Carrinho</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.html">Minha Conta</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Autoajuda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Negócios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Finanças e Investimentos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mangás</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ver Tudo</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Pesquisar"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                <span class="material-symbols-outlined">search</span>
+              </button>
+            </form>
+          </div>
+        </div>
+    </nav>
+
+    <section class="carrinho-main">
+
+    </section>
+
+
+    <footer>
+        <h1 class="footer-titulo">Wissen</h1>
+        <div class="footer-links">
+          <a href="#" class="footer-item">Condições de Uso</a>
+          <a href="#" class="footer-item">Notificação de Privacidade</a>
+          <a href="#" class="footer-item">Cookies</a>
+          <a href="#" class="footer-item">Anúncios Baseados e Interesses</a>
+        </div>
+        <div class="footer-p">
+          <p>© 2020-2024 Wissen.com, Inc. ou suas afiliadas.</p>
+        </div>
+    </footer>
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
